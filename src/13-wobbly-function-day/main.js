6d186d8debe62bc05d9342f6f7b819b2736cd730
Philip Walsh <59885325+Philip-Walsh@users.noreply.github.com>
2024-01-19 01:50:52 +0000

    add new solutions

--- src/13-wobbly-function-day/main.js
+++ src/13-wobbly-function-day/main.js
@@ -1,1 +1,35 @@
 // TODO!
+var canvas = document.getElementById("sineWaveCanvas");
+var ctx = canvas.getContext("2d");
+
+function drawSineWave(amplitude = 50, frequency = 0.02) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    var x = 0;
+    var y = canvas.height / 2;
+    ctx.moveTo(x, y);
+
+    for (var i = 0; i < canvas.width; i++) {
+        x = i;
+        y = canvas.height / 2 + amplitude * Math.sin(frequency * x);
+        ctx.lineTo(x, y);
+    }
+    ctx.strokeStyle = "#009688";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+}
+
+drawSineWave();
+
+document.addEventListener('DOMContentLoaded', function () {
+    let amplitude;
+    let frequency;
+
+    const form = document.getElementById('values');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        amplitude = document.getElementById('amplitude').value;
+        frequency = document.getElementById('frequency').value;
+        drawSineWave(amplitude, frequency);
+    });
+});
--- src/17-draw-10-000-of-something/main.js
+++ src/17-draw-10-000-of-something/main.js
@@ -1,1 +1,18 @@
-// TODO!
+function getRandomDecimalEmoji() {
+    const minDecimal = 128512;
+    const maxDecimal = 127884;
+    const randomDecimal =
+        Math.floor(Math.random() * (maxDecimal - minDecimal + 1)) + minDecimal;
+    return String.fromCodePoint(randomDecimal);
+}
+
+function createAndAppendEmoji() {
+    const emoji = getRandomDecimalEmoji();
+    const emojiElement = document.createElement("span");
+    emojiElement.textContent = emoji;
+    document.body.appendChild(emojiElement);
+}
+
+for (let i = 0; i < 10000; i++) {
+    createAndAppendEmoji();
+}
